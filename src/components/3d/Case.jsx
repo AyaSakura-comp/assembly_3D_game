2bfb44686af58b463cf0b54ae273a0234dd28ae2
Developer <dev@local>
2026-02-21 22:29:48 +0800

    feat: refine Task 3 component positions and lighting

--- src/components/3d/Case.jsx
+++ src/components/3d/Case.jsx
@@ -2,23 +2,23 @@ export function ApexCase({ children }) {
   return (
     <group>
       {/* Main Wedge Body */}
-      <mesh position={[0, -2.8, 0]}>
+      <mesh position={[0, -0.2, 0]}>
         <boxGeometry args={[12, 0.8, 10]} />
         <meshStandardMaterial color="#e8e1d5" />
       </mesh>
       
       {/* Integrated Keyboard Slope */}
-      <mesh position={[0, -2.5, 4]} rotation={[-Math.PI / 10, 0, 0]}>
+      <mesh position={[0, 0.1, 4]} rotation={[-Math.PI / 10, 0, 0]}>
         <boxGeometry args={[11, 0.4, 4]} />
         <meshStandardMaterial color="#dcd3c1" />
       </mesh>
 
       {/* Side Vents / Details */}
-      <mesh position={[-6.1, -2.8, 0]}>
+      <mesh position={[-6.1, -0.2, 0]}>
         <boxGeometry args={[0.2, 0.4, 6]} />
         <meshStandardMaterial color="#999" />
       </mesh>
-      <mesh position={[6.1, -2.8, 0]}>
+      <mesh position={[6.1, -0.2, 0]}>
         <boxGeometry args={[0.2, 0.4, 6]} />
         <meshStandardMaterial color="#999" />
       </mesh>
--- src/components/3d/Motherboard.jsx
+++ src/components/3d/Motherboard.jsx
@@ -2,13 +2,13 @@ export function Motherboard({ children }) {
   return (
     <group>
       {/* Dark Green PCB Base */}
-      <mesh position={[0, -3, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+      <mesh position={[0, -0.4, 0]} rotation={[-Math.PI / 2, 0, 0]}>
         <planeGeometry args={[20, 20]} />
         <meshStandardMaterial color="#003322" />
       </mesh>
       
       {/* Decorative Motherboard Details (Edge) */}
-      <mesh position={[0, -2.99, -4.5]}>
+      <mesh position={[0, -0.39, -4.5]}>
         <boxGeometry args={[11.8, 0.1, 0.5]} />
         <meshStandardMaterial color="#004433" />
       </mesh>
